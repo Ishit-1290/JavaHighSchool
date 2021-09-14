9e2c8b55b4b8636d480977a76f7889c48de21b73
Ishit-1290 <vinee042006@gmail.com>
2021-09-14 19:09:14 +0530

    More program

--- Arrays/temp.java
+++ Arrays/temp.java
@@ -1,34 +1,17 @@
-import java.util.*;
-public class temp{
-    public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n-1; j++)
-                System.out.print(" ");
-            for(int j = i; j >= 0; j--)
-                System.out.print(j + 1);
+class temp
+{
+    public static void main(String[] args)
+    {
+        int spaces = 0;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < spaces; j++){
+                System.out.print("  ");
+            }
+            for(int j = 4; j >= i+1; j-- ){
+                System.out.print((j+1) + " ");
+            }
             System.out.println();
+            spaces++;
         }
-        sc.close();        
     }
 }
-/*
-1
-12
-123
-1234
-12345
-
-
-
-
-
-
-
-
-
-
-
-
-*/
