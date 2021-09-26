c995f234b06ea86f2f13e847f78a2625c847e3fa
Ishit-1290 <vinee042006@gmail.com>
2021-09-26 14:25:06 +0530

    All In One

--- Arrays/temp.java
+++ Arrays/temp.java
@@ -1,17 +1,6 @@
-class temp
-{
-    public static void main(String[] args)
-    {
-        int spaces = 0;
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < spaces; j++){
-                System.out.print("  ");
-            }
-            for(int j = 4; j >= i+1; j-- ){
-                System.out.print((j+1) + " ");
-            }
-            System.out.println();
-            spaces++;
-        }
+import java.util.*;
+class temp <T>{
+    public static void main(String[] args) {
+        
     }
 }
